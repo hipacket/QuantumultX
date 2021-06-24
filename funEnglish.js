04cf8baae1c09009e02a32abf6c9a4fa13773b00
hipacket <48793270+hipacket@users.noreply.github.com>
2021-06-24 09:05:21 +0700

    Update funEnglish.js

--- funEnglish.js
+++ funEnglish.js
@@ -1,6 +1,9 @@
 var obj = JSON.parse($response.body);
-obj[0].has_expiration = false;
-obj[0].expires_gmt = 4071200107;
-obj[0].expires_gmt_formatted = '2099-01-04 15:55:07';
+var plan = obj.find(p => p.has_expiration);
+if(plan) {
+  plan.has_expiration = false;
+  plan.expires_gmt = 4071200107;
+  plan.expires_gmt_formatted = '2099-01-04 15:55:07';
+}
 
 $done({body: JSON.stringify(obj)});
