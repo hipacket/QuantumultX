325758e5060278a08903a86725d478dce3d546a2
hipacket <48793270+hipacket@users.noreply.github.com>
2021-06-28 10:01:37 +0700

    Create list.js

---
+++ list.js
@@ -0,0 +1,2 @@
+//^https:\/\/apis\.littlefox\.com\/api\/v1\/contents\/C.+ url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/littlefox.js
+//^https/\/raw\.githubusercontent\.com\/hipacket\/QuantumultX\/main\/test\.json$ url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/littlefox.js
