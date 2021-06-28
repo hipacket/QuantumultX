e2a0389dd049a4984173fcb681631be8580d3915
hipacket <48793270+hipacket@users.noreply.github.com>
2021-06-28 10:01:20 +0700

    Update littlefox.js

--- littlefox.js
+++ littlefox.js
@@ -1,4 +1,3 @@
-//^https:\/\/apis\.littlefox\.com\/api\/v1\/contents\/C.+ url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/littlefox.js
 var obj = JSON.parse($response.body);
 delete obj.data.preview_time;
 
