178c231801d87f602b66a9a4006471fe140661d3
hipacket <48793270+hipacket@users.noreply.github.com>
2021-06-29 19:34:04 +0700

    Update list.js

--- list.js
+++ list.js
@@ -1,2 +1,3 @@
 //^https:\/\/apis\.littlefox\.com\/api\/v1\/contents\/C.+ url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/littlefox.js
 //^https/\/raw\.githubusercontent\.com\/hipacket\/QuantumultX\/main\/test\.json$ url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/littlefox.js
+//^https:\/\/subs\.studycat\.net\/membership-service\/1\/users\/memberships url script-response-body https://raw.githubusercontent.com/hipacket/QuantumultX/main/funEnglish.js
